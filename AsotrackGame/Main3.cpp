27fd177f11c5f789d0323e006603147d5cb91bf2
growinghsb <seungbeombm@gmail.com>
2021-10-15 22:10:30 +0900

    [ADD] today cpp study with make bingo game

--- AsotrackGame/Main3.cpp
+++ AsotrackGame/Main3.cpp
@@ -25,37 +25,50 @@ int main(void) {
 		board[idx2] = tmp;
 	}
 
-	for (size_t i = 0; i < 5; i++)
-	{
-		for (size_t j = 0; j < 5; j++)
-		{
-			// i * 5 = 시작지점이자 행을 지정, j = 하나하나 증가 하면서 해당 열을 출력
-			cout << board[i * 5 + j] << '\t';
-		}
-		cout << endl << endl;
-	}
-	cout << endl;
-	// 여기까지 초기 세팅
-
 	while (true) {
+		system("cls");
+
+		for (size_t i = 0; i < 5; i++)
+		{
+			for (size_t j = 0; j < 5; j++)
+			{
+				if (board[i * 5 + j] == 42) {
+					cout << '*' << '\t';
+					continue;
+				}
+				// i * 5 = 시작지점이자 행을 지정, j = 하나하나 증가 하면서 해당 열을 출력
+				cout << board[i * 5 + j] << '\t';
+			}
+			cout << endl << endl;
+		}
+		cout << endl;
+		// 여기까지 초기 세팅
+		
 		size_t inputNum;
 		cout << "빙고게임 입니다." << endl << "1 ~ 25까지의 숫자중 하나를 입력하세요!" << endl
-			<< "종료 하시려면 '0' or ctrl + z 를 입력하세요." << endl;
+			<< "종료 하시려면 ctrl + z 를 입력하세요." << endl;
 		cin >> inputNum;
 
-		if (inputNum == 0 || cin.eof()) {
+		if (cin.eof()) {
 			cout << "게임을 종료 합니다." << endl;
 			break;
 		}
 
-		if (inputNum > 25 || cin.fail()) {
+		if (inputNum == 0 || inputNum > 25) {
 			cin.setstate(ios::failbit);
-			cout << "잘못된 입력 입니다. 다시 입력하세요" << endl;
 			cin.clear();
-			cin.ignore(LLONG_MAX, '/n');
+			cin.ignore(UINT32_MAX, '\n'); // 첫 번째 매개변수 값이 너무 크면 에러가 난다. ULLONG_MAX 값으로 했을 때 에러가 났다.
 			continue;
 		}
-	}
+		cout << endl;
 
+		for (size_t i = 0; i < LENGTH; i++)
+		{
+			if (inputNum == board[i]) {
+				board[i] = '*';
+				break;
+			}
+		}
+	}
 	return 0;
 }

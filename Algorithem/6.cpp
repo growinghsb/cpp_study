311e65ec86d740762518106e6a5b13e02b14d627
growinghsb <seungbeombm@gmail.com>
2021-10-21 18:30:46 +0900

    [ADD] practice6 Mod ver

--- Algorithem/6.cpp
+++ Algorithem/6.cpp
@@ -9,45 +9,25 @@ int main(void) {
 
 	cin >> chArr;
 
-	size_t index = 0;
+	size_t result = 0;
 	while (*p != '\0') {
-		size_t tmp = *p - 48;
 
-		if (tmp > 0 && tmp < 10) {
-			chArr[index] = tmp + 48;
-			index++;
+		if (*p >= 48 && *p <= 57) {
+			result = result * 10 + (*p - 48);
 		}
-
-		if (tmp == 0 && index != 0) {
-			chArr[index] = tmp + 48;
-			index++;
-		}
-
 		p++;
 	}
 
-
-	size_t sum = 0;
-	for (size_t i = 0; i < index; i++)
-	{
-		size_t digit = 1;
-
-		for (size_t j = i; j < index - 1; j++) {
-			digit *= 10;
-		}
-
-		sum += (chArr[i] - 48) * digit;
-	}
-
 	size_t count = 0;
-	for (size_t i = 1; i < sum + 1; i++)
+	for (size_t i = 1; i < result + 1; i++)
 	{
-		if (sum % i == 0) {
+		if (result % i == 0) {
 			count++;
 		}
 	}
 
-	cout << sum << endl;
+	cout << result << endl;
 	cout << count;
+
 	return 0;
 }

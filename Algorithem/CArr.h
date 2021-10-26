8f94a643de4042d2bf8297445ad8d15b8c32872c
growinghsb <seungbeombm@gmail.com>
2021-10-26 11:25:17 +0900

    [ADD] apply template

--- Algorithem/CArr.h
+++ Algorithem/CArr.h
@@ -1,17 +1,24 @@
 #pragma once
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template<typename T>
 class CArr
 {
 public:
 	CArr();
-	CArr(const CArr& other);
+	CArr(const CArr<T>& other);
 	~CArr();
 
-	CArr& operator=(const CArr& other);
-	bool operator==(const CArr& other) const;
-	int& operator[](const unsigned int index) const;
+	CArr<T>& operator=(const CArr<T>& other);
+	bool operator==(const CArr<T>& other) const;
+	T& operator[](const unsigned int index) const;
 
-	void pushBack(const int data);
-	void pushIndex(const int data, const unsigned int index);
+	void pushBack(const T& data);
+	void pushIndex(const T& data, const unsigned int index);
 	void resize(const unsigned int size);
 	void remove(const unsigned int index);
 	unsigned int getCount() const;
@@ -19,10 +26,157 @@ public:
 	void print() const;
 
 private:
-	int* mArr;
+	T* mArr;
 	unsigned int mSize;
 	unsigned int mCount;
 
-	void dataCopy(int* target, const int* source, const unsigned int count);
+	void dataCopy(T* target, const T* source, const unsigned int count);
 };
 
+// 템플릿 클래스 함수 상세 구현
+
+template<typename T>
+CArr<T>::CArr()
+	: mSize(2) // 기본 정책은 공간 2개 만들기, 더 필요하면 resize() 이용
+	, mCount(0)
+{
+
+	mArr = new T[mSize];
+}
+
+template<typename T>
+CArr<T>::CArr(const CArr<T>& other)
+	: mSize(other.mSize)
+	, mCount(other.mCount)
+
+{
+	mArr = new T[mSize];
+
+	dataCopy(mArr, other.mArr, other.mCount);
+}
+
+template<typename T>
+CArr<T>::~CArr()
+{
+	delete[] mArr;
+}
+
+template<typename T>
+CArr<T>& CArr<T>::operator=(const CArr<T>& other)
+{
+	mSize = other.mSize;
+	mCount = other.mCount;
+
+	delete[] mArr;
+	mArr = new T[mSize];
+
+	dataCopy(mArr, other.mArr, other.mCount);
+
+	return *this;
+}
+
+template<typename T>
+bool CArr<T>::operator==(const CArr<T>& other) const
+{
+	if (mCount != other.mCount) {
+		return false;
+	}
+
+	for (unsigned int i = 0; i < mCount; i++)
+	{
+		if (*(mArr + i) != *(other.mArr + i)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template<typename T>
+T& CArr<T>:: operator[](const unsigned int index) const
+{
+	assert(mCount > index);
+
+	return *(mArr + index);
+}
+
+template<typename T>
+void CArr<T>::pushBack(const T& data)
+{
+	if (mSize == mCount)
+	{
+		resize(mSize * 2);
+	}
+
+	*(mArr + mCount) = data;
+	mCount++;
+}
+
+template<typename T>
+void CArr<T>::pushIndex(const T& data, const unsigned int index)
+{
+	assert(mCount > index);
+
+	if (mSize == mCount)
+	{
+		resize(mSize * 2);
+	}
+
+	*(mArr + mCount) = *(mArr + index);
+	*(mArr + index) = data;
+	mCount++;
+}
+
+template<typename T>
+void CArr<T>::resize(const unsigned int size)
+{
+	assert(mSize < size);
+
+	mSize = size;
+	T* p = new T[mSize];
+
+	dataCopy(p, mArr, mCount);
+
+	delete[] mArr;
+	mArr = p;
+}
+
+template<typename T>
+void CArr<T>::remove(const unsigned int index)
+{
+	assert(mCount > index);
+
+	*(mArr + index) = *(mArr + mCount - 1);
+	mCount--;
+}
+
+template<typename T>
+unsigned int CArr<T>::getCount() const
+{
+	return mCount;
+}
+
+template<typename T>
+unsigned int CArr<T>::getSize() const
+{
+	return mSize;
+}
+
+template<typename T>
+void CArr<T>::print() const
+{
+	for (unsigned int i = 0; i < mCount; i++)
+	{
+		cout << *(mArr + i) << "  ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void CArr<T>::dataCopy(T* target, const T* source, const unsigned int count)
+{
+	for (unsigned int i = 0; i < count; i++)
+	{
+		*(target + i) = *(source + i);
+	}
+}
--- Algorithem/CArrMain.cpp
+++ Algorithem/CArrMain.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(void) {
-	CArr arr;
+	CArr<int> arr;
 	cout << "size: " << arr.getSize() << endl;
 	cout << "count: " << arr.getCount() << endl;
 
@@ -48,18 +48,18 @@ int main(void) {
 	cout << "count: " << arr.getCount() << endl;
 	arr.print();
 
-	CArr arr1;
-	CArr arr2;
+	CArr<float> arr1;
+	CArr<float> arr2;
 
-	arr1.pushBack(1);
-	arr1.pushBack(2);
-	arr1.pushBack(3);
-	arr1.pushBack(4);
-	arr1.pushBack(5);
+	arr1.pushBack(1.45f);
+	arr1.pushBack(2.87f);
+	arr1.pushBack(3.09f);
+	arr1.pushBack(4.819f);
+	arr1.pushBack(5.99f);
 
-	arr2.pushBack(10);
-	arr2.pushBack(20);
-	arr2.pushBack(30);
+	arr2.pushBack(10.123f);
+	arr2.pushBack(20.391f);
+	arr2.pushBack(30.219f);
 
 	bool result = arr1 == arr2;
 	cout << boolalpha << "arr1 = arr2 진행 전: " << result << endl;
@@ -70,23 +70,23 @@ int main(void) {
 	cout << boolalpha << "arr1 = arr2 진행 후: " << result << endl;
 
 	//int arrNum3 = arr1[3]; 경계값 테스트
-	int arrNum1 = arr1[0];
-	int arrNum2 = arr1[1];
-	int arrNum3 = arr1[2];
+	float arrNum1 = arr1[0];
+	float arrNum2 = arr1[1];
+	float arrNum3 = arr1[2];
 	cout << arrNum1 << endl;
 	cout << arrNum2 << endl;
 	cout << arrNum3 << endl;
 
-	arr1[0] = 1000;
-	arr1[1] = 2000;
-	arr1[2] = 3000;
+	arr1[0] = 99.0923f;
+	arr1[1] = 99.1232f;
+	arr1[2] = 99.768f;
 	cout << arr1[0] << endl;
 	cout << arr1[1] << endl;
 	cout << arr1[2] << endl;
 
 	cout << endl;
 	cout << "복사생성자로 객체 생성" << endl;
-	CArr copy(arr1);
+	CArr<float> copy(arr1);
 	result = copy == arr1;
 	cout << boolalpha << "copy == arr1: " << result << endl;
 

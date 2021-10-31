c864362c80b2d74624b61bcaecdf78e4d1182c8c
growinghsb <seungbeombm@gmail.com>
2021-10-31 22:39:13 +0900

    [ADD] git today cpp study with map(binary search tree) finished

--- Algorithem/Map.h
+++ Algorithem/Map.h
@@ -89,6 +89,7 @@ public:
 	Node<KeyT, ValueT>* GetLeftChild() const;
 	Node<KeyT, ValueT>* GetRightChild() const;
 
+	void SetPair(MyPair<KeyT, ValueT> pair);
 	void SetParent(Node<KeyT, ValueT>* parent);
 	void SetLeftChild(Node<KeyT, ValueT>* leftChild);
 	void SetRightChild(Node<KeyT, ValueT>* rightChild);
@@ -140,6 +141,12 @@ inline Node<KeyT, ValueT>* Node<KeyT, ValueT>::GetRightChild() const
 }
 
 template<typename KeyT, typename ValueT>
+inline void Node<KeyT, ValueT>::SetPair(MyPair<KeyT, ValueT> pair)
+{
+	mPair = pair;
+}
+
+template<typename KeyT, typename ValueT>
 inline void Node<KeyT, ValueT>::SetParent(Node<KeyT, ValueT>* parent)
 {
 	mParent = parent;
@@ -166,16 +173,20 @@ private:
 	Node<KeyT, ValueT>* mRootNode;
 	unsigned int mCount;
 
+	void DeleteNode(Node<KeyT, ValueT>* node, Node<KeyT, ValueT>* successor);
+
 public:
 	MyMap();
 	~MyMap();
 
 	bool Insert(const MyPair<KeyT, ValueT>& pair);
+	Node<KeyT, ValueT>* GetInOrederSuccessor(Node<KeyT, ValueT>& cNode);
 
 	class Iterator;
 	MyMap<KeyT, ValueT>::Iterator begin();
 	MyMap<KeyT, ValueT>::Iterator end();
 	MyMap<KeyT, ValueT>::Iterator find(const KeyT& key);
+	MyMap<KeyT, ValueT>::Iterator erase(const MyMap<KeyT, ValueT>::Iterator& iter);
 
 	/*
 		inner class Iterator
@@ -190,6 +201,8 @@ public:
 		Iterator(MyMap<KeyT, ValueT>* map, Node<KeyT, ValueT>* node);
 		~Iterator();
 
+		MyMap<KeyT, ValueT>* GetMap() const;
+		Node<KeyT, ValueT>* GetNode() const;
 		void SetMap(MyMap<KeyT, ValueT>* map);
 		void SetNode(Node<KeyT, ValueT>* node);
 
@@ -224,6 +237,18 @@ inline MyMap<KeyT, ValueT>::Iterator::~Iterator()
 }
 
 template<typename KeyT, typename ValueT>
+inline MyMap<KeyT, ValueT>* MyMap<KeyT, ValueT>::Iterator::GetMap() const
+{
+	return mCMap;
+}
+
+template<typename KeyT, typename ValueT>
+inline Node<KeyT, ValueT>* MyMap<KeyT, ValueT>::Iterator::GetNode() const
+{
+	return mCNode;
+}
+
+template<typename KeyT, typename ValueT>
 inline void MyMap<KeyT, ValueT>::Iterator::SetMap(MyMap<KeyT, ValueT>* map)
 {
 	mCMap = map;
@@ -238,9 +263,20 @@ inline void MyMap<KeyT, ValueT>::Iterator::SetNode(Node<KeyT, ValueT>* node)
 template<typename KeyT, typename ValueT>
 inline bool MyMap<KeyT, ValueT>::Iterator::operator==(const MyMap<KeyT, ValueT>::Iterator& rhs) const
 {
-	assert(nullptr != mCMap && nullptr != mCNode);
+	assert(nullptr != mCMap);
 
-	if (mCMap->mRootNode == rhs.mCMap->mRootNode && mCNode->GetPair().GetKey() == rhs.mCNode->GetPair().GetKey())
+	if (nullptr == mCNode)
+	{
+		if (nullptr == rhs.mCNode)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	if (mCMap->mRootNode == rhs.mCMap->mRootNode
+		&& nullptr != rhs.mCNode
+		&& mCNode->GetPair().GetKey() == rhs.mCNode->GetPair().GetKey())
 	{
 		return true;
 	}
@@ -250,9 +286,20 @@ inline bool MyMap<KeyT, ValueT>::Iterator::operator==(const MyMap<KeyT, ValueT>:
 template<typename KeyT, typename ValueT>
 inline bool MyMap<KeyT, ValueT>::Iterator::operator!=(const MyMap<KeyT, ValueT>::Iterator& rhs) const
 {
-	assert(nullptr != mCMap && nullptr != mCNode);
+	assert(nullptr != mCMap);
 
-	if (mCMap->mRootNode != rhs.mCMap->mRootNode || mCNode->GetPair().GetKey() != rhs.mCNode->GetPair().GetKey())
+	if (nullptr == mCNode)
+	{
+		if (nullptr == rhs.mCNode)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	if (mCMap->mRootNode != rhs.mCMap->mRootNode
+		|| nullptr == rhs.mCNode
+		|| mCNode->GetPair()->GetKey() != rhs.mCNode->GetPair()->GetKey())
 	{
 		return true;
 	}
@@ -276,20 +323,14 @@ inline const MyPair<KeyT, ValueT>& MyMap<KeyT, ValueT>::Iterator::operator*() co
 }
 
 template<typename KeyT, typename ValueT>
-inline MyMap<KeyT, ValueT>::Iterator& MyMap<KeyT, ValueT>::Iterator::operator++()
+inline typename MyMap<KeyT, ValueT>::Iterator& MyMap<KeyT, ValueT>::Iterator::operator++()
 {
 	/*
 		중위후속자: 자신의 다음 순서 - next
 		중위선행자: 자신의 이전 순서 - prev
-
-		선행자는 출력하지 않는다. 
-		다음 반환되는 iterator 에서는 후속자를 가르킨다. 
-		iter가 최상위루트면, 출력 후 오른쪽자식으로
-		한 번 이동 후 쭉 왼쪽자식으로 내려가 가장 작은
-		node 를 가르키는 iter 를 반환한다.
-		그리고 똑같이 선행자는 출력하지 않고, 
-		후속자만 가르키면 된다. 
 	*/
+	this->SetNode(this->mCMap->GetInOrederSuccessor(*(this->mCNode)));
+	return *this;
 }
 
 /*
@@ -355,6 +396,50 @@ inline bool MyMap<KeyT, ValueT>::Insert(const MyPair<KeyT, ValueT>& pair)
 	return true;
 }
 
+/*
+	중위후속자를 반환하는 함수
+	중위후속자란 중위순회에서 다음
+	출력할 노드를 말한다. 다음 순서의
+	노드를 말한다.
+
+	& 로 매개변수를 받으면 nullptr 이 들어올 수 없다.
+	해서 nullptr 체크를 하지 않아도 된다.
+*/
+template<typename KeyT, typename ValueT>
+inline Node<KeyT, ValueT>* MyMap<KeyT, ValueT>::GetInOrederSuccessor(Node<KeyT, ValueT>& cNode)
+{
+	Node<KeyT, ValueT>* target = nullptr;
+
+	if (nullptr != cNode.GetRightChild())
+	{
+		target = cNode.GetRightChild();
+
+		while (nullptr != target && nullptr != target->GetLeftChild())
+		{
+			target = target->GetLeftChild();
+		}
+	}
+	else
+	{
+		target = cNode.GetParent();
+
+		while (true)
+		{
+			if (nullptr == target)
+			{
+				break;
+			}
+
+			if (cNode.GetPair()->GetKey() < target->GetPair()->GetKey())
+			{
+				break;
+			}
+			target = target->GetParent();
+		}
+	}
+	return target;
+}
+
 template<typename KeyT, typename ValueT>
 inline typename MyMap<KeyT, ValueT>::Iterator MyMap<KeyT, ValueT>::begin()
 {
@@ -392,4 +477,78 @@ inline typename MyMap<KeyT, ValueT>::Iterator MyMap<KeyT, ValueT>::find(const Ke
 		target->GetPair()->GetKey() < key ? target = target->GetRightChild() : target = target->GetLeftChild();
 	}
 	return iter;
+}
+
+template<typename KeyT, typename ValueT>
+inline typename MyMap<KeyT, ValueT>::Iterator MyMap<KeyT, ValueT>::erase(const MyMap<KeyT, ValueT>::Iterator& iter)
+{
+	/* 세 가지 경우
+		1. 리프노드를 지울 때
+		2. 자식이 하나 있는 노드를 지울 때
+		3. 자식이 두 개 있는 노드를 지울 때
+	*/
+	Node<KeyT, ValueT>* successor = GetInOrederSuccessor(*(iter.GetNode()));
+
+	if (nullptr == iter.GetNode()->GetLeftChild() && nullptr == iter.GetNode()->GetRightChild())
+	{
+		DeleteNode(iter.GetNode(), nullptr);
+	}
+	else if (nullptr != iter.GetNode()->GetLeftChild() && nullptr != iter.GetNode()->GetRightChild())
+	{
+		Node<KeyT, ValueT>* targetNode = iter.GetNode();
+
+		KeyT successorKey = successor->GetPair()->GetKey();
+		ValueT successorValue = successor->GetPair()->GetValue();
+
+		nullptr != successor->GetRightChild()
+			? DeleteNode(successor, successor->GetRightChild())
+			: DeleteNode(successor, nullptr);
+
+		targetNode->SetPair(MyPair<KeyT, ValueT>(successorKey, successorValue));
+		successor = targetNode;
+	}
+	else
+	{
+		nullptr != iter.GetNode()->GetLeftChild()
+			? DeleteNode(iter.GetNode(), iter.GetNode()->GetLeftChild())
+			: DeleteNode(iter.GetNode(), iter.GetNode()->GetRightChild());
+	}
+	--mCount;
+
+	return MyMap<KeyT, ValueT>::Iterator(this, successor);
+}
+
+template<typename KeyT, typename ValueT>
+inline void MyMap<KeyT, ValueT>::DeleteNode(Node<KeyT, ValueT>* node, Node<KeyT, ValueT>* successor)
+{
+	Node<KeyT, ValueT>* parent = node->GetParent();
+
+	// 데이터가 없는 루트노드일 때
+	if (nullptr == successor && nullptr == parent)
+	{
+		mRootNode = successor;
+	}
+	// 데이터가 있는 루트노드일 때
+	else if (nullptr != successor && nullptr == parent)
+	{
+		successor->SetParent(nullptr);
+		mRootNode = successor;
+	}
+	else
+	{
+		if (nullptr != successor)
+		{
+			successor->SetParent(parent);
+		}
+
+		if (node->GetPair()->GetKey() < parent->GetPair()->GetKey())
+		{
+			parent->SetLeftChild(successor);
+		}
+		else
+		{
+			parent->SetRightChild(successor);
+		}
+	}
+	delete node;
 }
--- Algorithem/MapMain.cpp
+++ Algorithem/MapMain.cpp
@@ -9,6 +9,8 @@ using std::endl;
 using std::map;
 using std::pair;
 
+template<typename KeyT, typename ValueT>
+void printMap(MyMap<KeyT, ValueT> map, typename MyMap<KeyT, ValueT>::Iterator iter);
 
 int main(void) {
 	srand(static_cast<int>(time(0)));
@@ -57,17 +59,86 @@ int main(void) {
 	{
 		myMap.Insert(MakeMyPair<int, int>(keys[i], i + 1));
 	}
-	myMap.Insert(MakeMyPair<int, int>(1001, 10000));
-	myMap.Insert(MakeMyPair<int, int>(2001, 20000));
+	myMap.Insert(MakeMyPair<int, int>(keys[500], 10000));
+	myMap.Insert(MakeMyPair<int, int>(keys[550], 20000));
 
 	MyMap<int, int>::Iterator iter1 = myMap.begin();
 	MyMap<int, int>::Iterator iter2 = myMap.end();
 
-	MyMap<int, int>::Iterator result1 = myMap.find((int)1001);
-	MyMap<int, int>::Iterator result2 = myMap.find((int)2001);
+	MyMap<int, int>::Iterator result1 = myMap.find((int)keys[500]);
+	MyMap<int, int>::Iterator result2 = myMap.find((int)keys[550]);
 
 	cout << iter1->GetKey() << endl; // pair* 반환해서 ->연산자로 pair 접근
 	cout << (*iter1).GetKey() << endl; // pair& 반환해서 ()로 묶어 pair& 가져오고 .연산자로 접근
 
+	printMap(myMap, iter1);
+	cout << endl << endl;
+
+	cout << "지우는 키: " << keys[500] << endl;
+	iter1 = myMap.erase(myMap.find((int)keys[500]));
+	cout << "지우는 키 다음 키: " << iter1->GetKey() << endl;
+	cout << endl << endl;
+
+	cout << "지우는 키: " << keys[550] << endl;
+	iter1 =	myMap.erase(myMap.find((int)keys[550]));
+	cout << "지우는 키 다음 키: " << iter1->GetKey() << endl;
+	cout << endl << endl;
+
+	printMap(myMap, myMap.begin());
+	cout << endl << endl;
+
+	MyMap<int, int> testMap;
+	testMap.Insert(MakeMyPair((int)100, (int)0));
+	testMap.Insert(MakeMyPair((int)150, (int)0));
+	testMap.Insert(MakeMyPair((int)80, (int)0));
+	testMap.Insert(MakeMyPair((int)90, (int)0));
+	testMap.Insert(MakeMyPair((int)65, (int)0));
+	testMap.Insert(MakeMyPair((int)120, (int)0));
+	testMap.Insert(MakeMyPair((int)160, (int)0));
+	testMap.Insert(MakeMyPair((int)95, (int)0));
+	testMap.Insert(MakeMyPair((int)130, (int)0));
+
+	/*
+							100
+					80				150
+				65		90		120		160								
+					      95      130
+	*/
+
+	MyMap<int, int>::Iterator testIter(testMap.begin());
+	printMap(testMap, testMap.begin());
+	
+	cout << endl;
+
+	testIter = testMap.erase(testMap.find(65));
+	testIter = testMap.erase(testMap.find(120));
+	testIter = testMap.erase(testMap.find(90));
+	testIter = testMap.erase(testMap.find(150));
+
+	/*
+							100
+					80				160
+						95		130		
+						        
+	*/
+
 	return 0;
+}
+
+template<typename KeyT, typename ValueT>
+void printMap(MyMap<KeyT, ValueT> map, typename MyMap<KeyT, ValueT>::Iterator iter)
+{
+	unsigned int count = 0;
+	while (iter != map.end())
+	{
+		if (count == 10)
+		{
+			cout << endl;
+			count = 0;
+		}
+		cout << "[" << iter->GetKey() << "]";
+
+		++iter;
+		++count;
+	}
 }

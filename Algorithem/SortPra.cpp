e81a5c5ffe6e37c4d6f0b91269c3c98612308651
growinghsb <seungbeombm@gmail.com>
2021-11-05 19:05:51 +0900

    [ADD] merge sort finish

--- Algorithem/SortPra.cpp
+++ Algorithem/SortPra.cpp
@@ -9,6 +9,9 @@
 
 #include <iostream>
 
+const unsigned int SIZE = 8;
+static int sorted[SIZE] = {};
+
 using std::cout;
 using std::endl;
 
@@ -16,16 +19,21 @@ void Swap(int& arr1, int& arr2);
 
 void BubbleSort(int* arr, unsigned int size);
 void SelectionSort(int* arr, unsigned int size);
+void InsertionSort(int* arr, unsigned int size);
+
+void MergeSort(int* arr, int start, int end);
+void Merge(int* arr, int start, int mid, int end);
 
 void print(int* arr, unsigned int size);
 
 int main(void)
 {
-	const unsigned int SIZE = 10;
-	int arr[10] = { 10, 4, 8, 5, 6, 9, 13, 1, 3, 2 };
+	int arr[SIZE] = { 10, 4, 8, 5, 6, 9, 13, 2 };
 
-	// BubbleSort(arr, SIZE); 구현 완료 O(n * n)
+	// BubbleSort(arr, SIZE);    구현 완료 O(n * n)
 	// SelectionSort(arr, SIZE); 구현 완료 O(n * n)
+	// InsertionSort(arr, SIZE); 구현 완료 O(n * n)
+	MergeSort(arr, 0, SIZE - 1);
 	print(arr, SIZE);
 
 	return 0;
@@ -61,12 +69,88 @@ void SelectionSort(int* arr, unsigned int size)
 		unsigned int minIndex = i;
 		for (unsigned int j = i + 1; j < size; j++)
 		{
-			if (*(arr + minIndex) > *(arr + j)) 
+			if (*(arr + minIndex) > *(arr + j))
 			{
 				minIndex = j;
 			}
 		}
 		Swap(*(arr + i), *(arr + minIndex));
+	}
+}
+
+void InsertionSort(int* arr, unsigned int size)
+{
+	for (unsigned int i = 1; i < size; i++)
+	{
+		int target = *(arr + i);
+		int startIndex = i - 1;
+
+		while (startIndex != -1)
+		{
+			if (target < *(arr + startIndex))
+			{
+				*(arr + startIndex + 1) = *(arr + startIndex);
+			}
+			else
+			{
+				break;
+			}
+			--startIndex;
+		}
+		*(arr + startIndex + 1) = target;
+	}
+}
+
+void MergeSort(int* arr, int start, int end)
+{
+	if (start < end)
+	{
+		int midIndex = (start + end) / 2;
+
+		MergeSort(arr, start, midIndex);
+		MergeSort(arr, midIndex + 1, end);
+		Merge(arr, start, midIndex, end);
+	}
+}
+
+void Merge(int* arr, int start, int mid, int end)
+{
+	int part1 = start;
+	int part2 = mid + 1;
+	int sortedIndex = start;
+
+ 	while (part1 <= mid && part2 <= end)
+	{
+		if (*(arr + part1) < *(arr + part2))
+		{
+			sorted[sortedIndex++] = *(arr + part1);
+			++part1;
+		}
+		else 
+		{
+			sorted[sortedIndex++] = *(arr + part2);
+			++part2;
+		}
+	}
+	
+	if (part1 > mid) 
+	{
+		for (int i = part2; i < end + 1; i++)
+		{
+			sorted[sortedIndex++] = *(arr + i);
+		}
+	}
+	else
+	{
+		for (int i = part1; i < mid + 1; i++)
+		{
+			sorted[sortedIndex++] = *(arr + part1);
+		}
+	}
+
+	for (int i = start; i < end + 1; i++)
+	{
+		*(arr + i) = *(sorted + i);
 	}
 }
 

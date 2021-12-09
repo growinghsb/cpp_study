13d1ad5b1c2a37bfdad2a36ddeb49baf57128c03
growinghsb <seungbeombm@gmail.com>
2021-12-09 14:16:02 +0900

    [ADD] MyMap class find func, value find with key

--- Cplusplus/MyMap.cpp
+++ Cplusplus/MyMap.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
+#include "MyMap.h"
 #include <map>
 
-#include "MyMap.h"
-
-using std::cout;
-using std::endl;
 using std::map;
 using std::pair;
 using std::make_pair;
 
 int main() 
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	map<int, int> stlMap;
 	pair<int, int> stlPair;
 
@@ -23,20 +21,67 @@ int main()
 	int b = 40;
 
 	stlMap.insert(make_pair(a, b));
-
 	/*
 		MyMap
 	*/
-	MyPair<int, int> myPair;
-	myPair.mKey = 100;
-	myPair.mValue = 10;
 
-	MyPair<int, int> myPairCopy = myPair;
-	MyPair<int, int> myPairConstructor(200, 20);
-	myPairConstructor = myPair;
-	myPair = myPair;
-	MyPair<int, int> makePairFunc = makePair(300, 30);
-	makePairFunc = std::move(myPairConstructor);
+	//MyPair<int, int> myPair;
+	//myPair.mKey = 100;
+	//myPair.mValue = 10;
+
+	//MyPair<int, int> myPairCopy = myPair;
+	//MyPair<int, int> myPairConstructor(200, 20);
+	//myPairConstructor = myPair;
+	//myPair = myPair;
+	//MyPair<int, int> makePairFunc = makePair(300, 30);
+	//makePairFunc = move(myPairConstructor);
+
+	/*
+						100
+				80				130	
+			65		90		110		150
+	 	50		 85	  95  105			170	     
+	*/
+
+	MyMap<int, int> myMap;
+	
+	MyPair<int, int> myPair1(100, 1);
+	
+	MyPair<int, int> myPair2;
+	myPair2.mKey = 80;
+	myPair2.mValue = 2;
+
+	MyPair<int, int> myPair3 = makePair(130, 3);
+
+	MyPair<int, int> myPair4;
+	myPair4 = makePair(65, 4);
+
+	MyPair<int, int> rValuePair(90, 5);
+	MyPair<int, int> myPair5;
+	myPair5 = move(rValuePair);
+
+	MyPair<int, int> copyPair(110, 6);
+	MyPair<int, int> myPair6 = copyPair;
+
+	MyPair<int, int> copyPairRValue(150, 7);
+	MyPair<int, int> myPair7 = move(copyPairRValue);
+
+	myMap.insert(myPair1);
+	myMap.insert(myPair2);
+	myMap.insert(myPair3);
+	myMap.insert(myPair4);
+	myMap.insert(myPair5);
+	myMap.insert(myPair6);
+	myMap.insert(myPair7);
+	myMap.insert(makePair(50, 8));
+	myMap.insert(makePair(85, 9));
+	myMap.insert(makePair(95, 10));
+	myMap.insert(makePair(105, 11));
+	myMap.insert(makePair(170, 12));
+	myMap.insert(makePair(170, 1000));
+
+	int result1 = myMap.find(50);
+	int result2 = myMap.find(myPair7.mKey);
 
 	return 0;
 }
--- Cplusplus/MyMap.h
+++ Cplusplus/MyMap.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include<cassert>
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::move;
+
+/*
+	MyPair class
+*/
 template<typename KEY, typename VALUE>
-class MyPair
+class MyPair final
 {
 public:
 	MyPair();
@@ -10,7 +20,7 @@ public:
 	MyPair(MyPair<KEY, VALUE>&& other) noexcept;
 	~MyPair() = default;
 
-	void operator=(const MyPair<KEY, VALUE>& rhs) 
+	void operator=(const MyPair<KEY, VALUE>& rhs)
 	{
 		if (this == &rhs)
 		{
@@ -27,7 +37,7 @@ public:
 		{
 			return;
 		}
-		
+
 		mKey = rhs.mKey;
 		mValue = rhs.mValue;
 
@@ -39,15 +49,60 @@ public:
 	VALUE mValue;
 };
 
-class MyMap
+/*
+	MyNode class
+*/
+template<typename KEY, typename VALUE>
+class MyNode final
 {
+	template<typename KEY, typename VALUE>
+	friend class MyMap;
+public:
+	MyNode() = delete;
+
+private:
+	MyNode(MyPair<KEY, VALUE>& pairData);
+	~MyNode() = default;
+
+	MyPair<KEY, VALUE> mPairData;
+	MyNode<KEY, VALUE>* mParentNode;
+	MyNode<KEY, VALUE>* mLeftChildNode;
+	MyNode<KEY, VALUE>* mRightChildNode;
+};
+
+/*
+	MyMap class
+*/
+template<typename KEY, typename VALUE>
+class MyMap final
+{
+public:
+	MyMap();
+	~MyMap();
+
+	void insert(MyPair<KEY, VALUE>& pairData);
+	void insert(MyPair<KEY, VALUE>&& pairData);
+
+	VALUE& find(const KEY& key);
+	VALUE& find(const KEY&& key);
+
+	void clear();
+
+	int getCount() const
+	{
+		return mCount;
+	}
+
+private:
+	MyNode<KEY, VALUE>* mRootNode;
+	int mCount;
 };
 
 /*
 	makePair func
 */
 template<typename KEY, typename VALUE>
-MyPair<KEY, VALUE> makePair(KEY key, VALUE value) 
+MyPair<KEY, VALUE> makePair(KEY key, VALUE value)
 {
 	return MyPair<KEY, VALUE>(key, value);
 }
@@ -56,31 +111,152 @@ MyPair<KEY, VALUE> makePair(KEY key, VALUE value)
 	Mypair class func
 */
 template<typename KEY, typename VALUE>
-inline MyPair<KEY, VALUE>::MyPair()
+MyPair<KEY, VALUE>::MyPair()
 	: mKey(0)
 	, mValue(0)
 {
 }
 
 template<typename KEY, typename VALUE>
-inline MyPair<KEY, VALUE>::MyPair(KEY key, VALUE value)
+MyPair<KEY, VALUE>::MyPair(KEY key, VALUE value)
 	: mKey(key)
 	, mValue(value)
 {
 }
 
 template<typename KEY, typename VALUE>
-inline MyPair<KEY, VALUE>::MyPair(const MyPair<KEY, VALUE>& other)
+MyPair<KEY, VALUE>::MyPair(const MyPair<KEY, VALUE>& other)
 	: mKey(other.mKey)
 	, mValue(other.mValue)
 {
 }
 
 template<typename KEY, typename VALUE>
-inline MyPair<KEY, VALUE>::MyPair(MyPair<KEY, VALUE>&& other) noexcept
+MyPair<KEY, VALUE>::MyPair(MyPair<KEY, VALUE>&& other) noexcept
 	: mKey(other.mKey)
 	, mValue(other.mValue)
 {
 	other.mKey = 0;
 	other.mValue = 0;
+}
+
+/*
+	MyNode class func
+*/
+template<typename KEY, typename VALUE>
+MyNode<KEY, VALUE>::MyNode(MyPair<KEY, VALUE>& pairData)
+	: mPairData(pairData)
+	, mParentNode(nullptr)
+	, mLeftChildNode(nullptr)
+	, mRightChildNode(nullptr)
+{
+
+}
+
+/*
+	MyMap class func
+*/
+template<typename KEY, typename VALUE>
+MyMap<KEY, VALUE>::MyMap()
+	: mRootNode(nullptr)
+	, mCount(0)
+{
+}
+
+template<typename KEY, typename VALUE>
+MyMap<KEY, VALUE>::~MyMap()
+{
+}
+
+template<typename KEY, typename VALUE>
+void MyMap<KEY, VALUE>::insert(MyPair<KEY, VALUE>& pairData)
+{
+	MyNode<KEY, VALUE>* newNode = new MyNode<KEY, VALUE>(pairData);
+	++mCount;
+
+	if (nullptr == mRootNode)
+	{
+		mRootNode = newNode;
+	}
+	else
+	{
+		MyNode<KEY, VALUE>* targetNode = mRootNode;
+
+		while (nullptr != targetNode)
+		{
+			if (pairData.mKey > targetNode->mPairData.mKey)
+			{
+				if (nullptr == targetNode->mRightChildNode)
+				{
+					targetNode->mRightChildNode = newNode;
+					newNode->mParentNode = targetNode;
+					break;
+				}
+
+				targetNode = targetNode->mRightChildNode;
+			}
+			else if (pairData.mKey < targetNode->mPairData.mKey)
+			{
+				if (nullptr == targetNode->mLeftChildNode)
+				{
+					targetNode->mLeftChildNode = newNode;
+					newNode->mParentNode = targetNode;
+					break;
+				}
+
+				targetNode = targetNode->mLeftChildNode;
+			}
+
+			if (pairData.mKey == targetNode->mPairData.mKey)
+			{
+				--mCount;
+				targetNode->mPairData = move(newNode->mPairData);
+				delete newNode;
+
+				break;
+			}
+		}
+	}
+}
+
+template<typename KEY, typename VALUE>
+void MyMap<KEY, VALUE>::insert(MyPair<KEY, VALUE>&& pairData)
+{
+	insert(pairData);
+}
+
+template<typename KEY, typename VALUE>
+VALUE& MyMap<KEY, VALUE>::find(const KEY& key)
+{
+	assert(nullptr != mRootNode);
+
+	MyNode<KEY, VALUE>* startNode = mRootNode;
+
+	while (nullptr != startNode) 
+	{
+		if (key > startNode->mPairData.mKey) 
+		{
+			startNode = startNode->mRightChildNode;
+			continue;
+		}
+		else if (key < startNode->mPairData.mKey) 
+		{
+			startNode = startNode->mLeftChildNode;
+			continue;
+		}	
+		
+		return startNode->mPairData.mValue;
+	}
+}
+
+template<typename KEY, typename VALUE>
+VALUE& MyMap<KEY, VALUE>::find(const KEY&& key)
+{
+	return find(key);
+}
+
+template<typename KEY, typename VALUE>
+void MyMap<KEY, VALUE>::clear()
+{
+	
 }
